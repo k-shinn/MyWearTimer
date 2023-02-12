0bb0b094aa3338c5df2488f6c001362cb1299674
k-shinn <k.shinn0721@gmail.com>
2023-02-12 16:11:38 +0900

    タイマー起動中はScreenOnに。不要なプレビュー削除

--- app/src/main/java/com/kei/myweartimer/presentation/MainActivity.kt
+++ app/src/main/java/com/kei/myweartimer/presentation/MainActivity.kt
@@ -1,12 +1,11 @@
 package com.kei.myweartimer.presentation
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.tooling.preview.Devices
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
 import com.kei.myweartimer.data.DataStore
 import com.kei.myweartimer.presentation.theme.MyWearTimerTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -59,11 +58,16 @@ class MainActivity : ComponentActivity() {
                 )
             }
         }
-    }
-}
 
-@Preview(device = Devices.WEAR_OS_SMALL_ROUND, showSystemUi = true)
-@Composable
-fun DefaultPreview() {
-    // todo
+        // タイマー起動中はScreenOn
+        lifecycleScope.launch {
+            dataStore.isTimerRunning.collect {
+                if (it) {
+                    window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+                } else {
+                    window.clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+                }
+            }
+        }
+    }
 }
